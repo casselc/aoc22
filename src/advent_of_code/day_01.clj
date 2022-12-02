dd630ec46c172834adb9a7c7e6946ab452c5ebe5
Chuck Cassel <chuck.cassel@gmail.com>
2022-12-01 23:47:27 -0500

    badges

--- src/advent_of_code/day_01.clj
+++ src/advent_of_code/day_01.clj
@@ -1,7 +1,12 @@
 ;; # 🎄 Advent of Code: Day 1
+^{:nextjournal.clerk/visibility {:code :hide}}
 (ns advent-of-code.day-01
   (:require [nextjournal.clerk :as clerk]
+            [advent-of-code.index :refer [badge]]
             [clojure.java.io :as io]))
+
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/html (badge 2022))
 
 (def input (->> "data/day01.txt"
                 io/reader
@@ -22,5 +27,3 @@
                  sort
                  (take-last 3)
                  (apply +)))
-
-
--- src/advent_of_code/index.clj
+++ src/advent_of_code/index.clj
@@ -1,14 +1,46 @@
-;; # 🎄 Advent of Code
-
-;; [Advent of Code](https://adventofcode.com) with
-;; [Clerk](https://clerk.vision).
+;; # 🎄 Advent of Code 2022
 (ns advent-of-code.index
   {:nextjournal.clerk/visibility {:code :hide :result :hide}}
   (:require [babashka.fs :as fs]
+            [clj-http.client :as client]
             [clojure.string :as str]
-            [nextjournal.clerk :as clerk]))
+            [nextjournal.clerk :as clerk]
+            [hickory.core :as h]
+            [hickory.select :as s]))
 
-#_(days-with-contents)
+(defn badge
+  [year]
+  (let [stars (or (when-let [token (System/getenv "AOC_SESSION_TOKEN")]
+                    (->> (client/get (str "https://adventofcode.com/" year) {:headers {"Cookie" (str "session=" token)}})
+                         :body
+                         h/parse
+                         h/as-hickory
+                         (s/select (s/class "star-count"))
+                         first
+                         :content
+                         first))
+                  "0*")
+        title (str year ": " stars)]
+    [:svg {:xmlns "http://www.w3.org/2000/svg", :xmlns:xlink "http://www.w3.org/1999/xlink", :viewbox "0,0,85,20", :role "img", :aria-label title}
+     [:title title]
+     [:lineargradient {:id "s", :x2 "0", :y2 "100%"}
+      [:stop {:offset "0", :stop-color "#aaa", :stop-opacity ".2"}]
+      [:stop {:offset "1", :stop-color "#333", :stop-opacity ".2"}]]
+     [:clipPath {:id "r"}
+      [:rect {:width "85", :height "20", :rx "4", :fill "#fff"}]]
+     [:g {:clip-path "url(#r)"}
+      [:rect {:width "54", :height "20", :fill "#0a0e25"}]
+      [:rect {:x "54", :width "31", :height "20", :fill "#00cc00"}]
+      [:rect {:width "85", :height "20", :fill "url(#s)"}]]
+     [:g {:fill "#fff", :text-anchor "middle", :font-family "Verdana,Geneva,DejaVu Sans,sans-serif", :text-rendering "geometricPrecision", :font-size "110"}
+      [:g
+       (for [i (range 5)
+             :let [deg (* i 72)]]
+         [:line {:x1 "12", :y1 "10", :x2 "12", :y2 "4", :stroke "yellow", :stroke-width "2" :transform (str "rotate(" deg ", 12, 10)")}])]
+      [:text {:aria-hidden "true", :x "365", :y "140", :fill "#010101", :fill-opacity ".3", :transform "scale(.1) translate(4,4)", :textlength "270"} year]
+      [:text {:x "365", :y "140", :transform "scale(.1)", :fill "#fff", :textlength "270"} year]
+      [:text {:aria-hidden "true", :x "685", :y "140", :fill "#010101", :fill-opacity ".3", :transform "scale(.1) translate(4, 4)", :textlength "210"} stars]
+      [:text {:x "685", :y "140", :transform "scale(.1)", :fill "#fff", :textlength "210"} stars]]]))
 
 (defn build-paths
   "Computes the paths to build by looking for files in
@@ -23,13 +55,14 @@
                     (str f)))))
         (range 25)))
 
-
-#_(build-paths)
-
 {:nextjournal.clerk/visibility {:result :show}}
 
 ^::clerk/no-cache
-(clerk/html (into [:ul] (mapv (fn [path]
-                                (when-let [day (second (re-matches #".*day_(\d+).clj" path))]
-                                  [:li [:a {:href (clerk/doc-url path)} "Day " day]])) (build-paths))))
+(clerk/html
+ [:article
+  [:h2 "Solutions"]
+  (into [:ul] (mapv (fn [path]
+                      (when-let [day (second (re-matches #".*day_(\d+).clj" path))]
+                        [:li [:a {:href (clerk/doc-url path)} "Day " day]])) (build-paths)))
+  [:footer (badge 2022)]])
 
