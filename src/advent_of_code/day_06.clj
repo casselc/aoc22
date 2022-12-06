71bc01dd492cad6be73a9a5b9ef56ab318469ae4
Chuck Cassel <chuck.cassel@gmail.com>
2022-12-06 00:42:53 -0500

    day 6

--- src/advent_of_code/day_06.clj
+++ src/advent_of_code/day_06.clj
@@ -1,3 +1,27 @@
 ;; # 🎄 Advent of Code: Day 6
 (ns advent-of-code.day-06
-  (:require [nextjournal.clerk :as clerk]))
+  (:require
+   [clojure.java.io :as io]
+   [clojure.set :refer [intersection]]
+   [clojure.string :as string]
+   [nextjournal.clerk :as clerk]))
+
+(def input (->> "data/day06.txt"
+                io/reader
+                line-seq
+                first))
+
+(def part-1 (->> input
+                 (partition 4 1)
+                 (map distinct)
+                 (take-while #(< (count %) 4))
+                 count
+                 (+ 4)))
+
+
+(def part-2 (->> input
+                 (partition 14 1)
+                 (map distinct)
+                 (take-while #(< (count %) 14))
+                 count
+                 (+ 14)))
